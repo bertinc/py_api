55533f12e78febf072702e79ea35ead18dbfd594
bertinc <bertinc@gmail.com>
2026-02-06 13:24:31 -0700

    enableing cors

--- timesheet.py
+++ timesheet.py
@@ -1,7 +1,9 @@
 import sqlite3
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app) # enable CORS for all routes and origins
 
 @app.route('/addrow')
 def addrow():
