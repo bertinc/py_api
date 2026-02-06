abf57136dbfe1b98e9516344ea5f9c95d0d286e0
bertinc <bertinc@gmail.com>
2026-02-06 13:34:21 -0700

    adding comments for the CORS and localhost issues

--- timesheet.py
+++ timesheet.py
@@ -5,9 +5,17 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app) # enable CORS for all routes and origins
 
+# TypeError: Failed to fetch
+# To deal with CORS
+# On server: pip install Flask-CORS
+# In code: import CORS, enable all routes and origins
+
 @app.route('/addrow')
 def addrow():
     return "Row added!"
 
+# To access this api remotely on the nextwork
+# you must include the host as 0.0.0.0. This
+# allows flask to listen on more than just localhost.
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0', port=8001)
